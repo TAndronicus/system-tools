a93b3718ba202accfc523a8099843797bb93a22f
Jędrzej Biedrzycki <jedrzejjanbiedrzycki@gmail.com>
2018-06-13 19:14:50 +0200

    Initial commit

---
+++ Constants.py
@@ -0,0 +1,9 @@
+
+#  paths
+workspace_path = '/media/jb/145C91ED5C91CA3E/Workspace'
+study_docs_path = '/media/jb/145C91ED5C91CA3E/Dokumenty/Uczelnia'
+
+latex_catalog = workspace_path + '/LaTeX'
+
+#  extensions
+latex_output = ['.aux', '.bbl', '.blg', '.log', '.nav', '.out', '.snm', '.synctex.gz', '.toc']
---
+++ latex-tools.py
@@ -0,0 +1,24 @@
+from os import listdir, remove
+import os.path
+import Constants
+
+def delete_aux_files(path):
+    files = listdir(path)
+    for file in files:
+        if file.endswith('.aux'):
+            remove(path + '/' + file)
+
+def delete_everything_but_tex_and_pdf(path):
+    files = listdir(path)
+    for file in files:
+        if not(file.endswith('.tex') or file.endswith('.pdf') or file.endswith('.txt') or os.path.isdir(path + '/' + file)):
+            remove(path + '/' + file)
+
+def delete_files_by_extensions(path: str, extensions: []):
+    files = listdir(path)
+    for file in files:
+        for ext in extensions:
+            if file.endswith(ext):
+                remove(path + '/' + file)
+
+delete_files_by_extensions(Constants.study_docs_path + '/Seminarium dyplomowe/3', Constants.latex_output)
