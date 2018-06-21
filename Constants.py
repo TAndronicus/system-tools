ad63495164071905253eabc80a95f992d184b03d
Jędrzej Biedrzycki <jedrzejjanbiedrzycki@gmail.com>
2018-06-21 20:11:25 +0200

    Fix bibliography removal

--- Constants.py
+++ Constants.py
@@ -6,4 +6,4 @@ study_docs_path = '/media/jb/145C91ED5C91CA3E/Dokumenty/Uczelnia'
 latex_catalog = workspace_path + '/LaTeX'
 
 #  extensions
-latex_output = ['.aux', '.bbl', '.blg', '.idx', '.lof', '.log', '.lol', '.lot', '.nav', '.out', '.snm', '.synctex.gz', '.toc']
+latex_output = ['.aux', '.blg', '.idx', '.lof', '.log', '.lol', '.lot', '.nav', '.out', '.snm', '.synctex.gz', '.toc']
