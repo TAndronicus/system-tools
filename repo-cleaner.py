47d16e963dba73e219fecb9f9a8bbebc47c791af
Jędrzej Biedrzycki <jed.biedrzycki@gmail.com>
2021-10-23 10:03:47 +0200

    Refactoring

--- repo-cleaner.py
+++ repo-cleaner.py
@@ -11,46 +11,60 @@ dry_run = False
 
 def check_and_clean(path):
     files = listdir(path)
+    if contains_only_files(files, path):
+        return
+    directories = filter_out_nones(files)
+    latest_version = get_latest_version(directories)
+    if latest_version is None:
+        clean_recursively(path, directories)
+    elif len(directories) == 1:
+        return
+    else:
+        print('Update ' + path.split(Constants.m2_path)[1])
+        for directory_name in directories:
+            if directory_name == latest_version:
+                continue
+            print(directory_name + ' (Has newer version: ' + latest_version + ')')
+            remove_unless_dry_run(path, directory_name)
+
+
+def contains_only_files(files, path):
     only_files = True
     for index, file in enumerate(files):
         if isdir('/'.join([path, file])):
             only_files = False
         else:
             files[index] = None
-    if only_files:
-        return
-
-    directories = [d for d in files if d is not None]
-    latest_version = check_if_versions(directories)
-    if latest_version is None:
-        for directory in directories:
-            check_and_clean('/'.join([path, directory]))
-    elif len(directories) == 1:
-        return
-    else:
-        print('Update ' + path.split(Constants.m2_path)[1])
-        for directory in directories:
-            if directory == latest_version:
-                continue
-            print(directory + ' (Has newer version: ' + latest_version + ')')
-            if not dry_run:
-                shutil.rmtree('/'.join([path, directory]))
+    return only_files
 
 
-def check_if_versions(directories):
+def filter_out_nones(files):
+    return [d for d in files if d is not None]
+
+
+def get_latest_version(directories):
     if len(directories) == 0:
         return None
-    latest_version = ''
-    for directory in directories:
+    latest_version = directories[0]
+    for current_directory_name in directories:
         try:
-            current_version = semver.VersionInfo.parse(directory)
+            current_version = semver.VersionInfo.parse(current_directory_name)
+            if current_version.compare(latest_version) > 0:
+                latest_version = current_directory_name
         except ValueError:
+            print('Could not parse version: ' + current_directory_name + '')
             return None
-        if latest_version == '':
-            latest_version = directory
-        if current_version.compare(latest_version) > 0:
-            latest_version = directory
     return latest_version
+
+
+def clean_recursively(path, directories):
+    for directory in directories:
+        check_and_clean('/'.join([path, directory]))
+
+
+def remove_unless_dry_run(path, directory_name):
+    if not dry_run:
+        shutil.rmtree('/'.join([path, directory_name]))
 
 
 if __name__ == '__main__':
